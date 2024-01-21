1514b37869bf70c6b43e733d666cbafbcf8060b4
Dne <valentinalvarez1998@gmail.com>
2024-01-21 05:44:10 -0300

    Se elimina código que no se estaba utilizando en CartNavbar.jsx

--- src/components/Checkout/Cart/Cart.jsx
+++ src/components/Checkout/Cart/Cart.jsx
@@ -232,8 +232,6 @@ const Cart = () => {
 
                   }
 
-
-
                   <CartResume cartTotal={cart.cartTotal} />
 
 
--- src/components/Checkout/Cart/CartNavbar/CartNavbar.jsx
+++ src/components/Checkout/Cart/CartNavbar/CartNavbar.jsx
@@ -3,37 +3,6 @@ import { Container, Grid, } from "@mui/material";
 
 const CartNavbar = (props) => {
 
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-    const [showSplitSections, setShowSplitSections] = useState(false);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleResize);
-        handleResize();
-
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    useEffect(() => {
-
-        const isLargeScreen = windowWidth <= 1200;
-
-        const isMeddiumScreen = windowWidth <= 992;
-
-        setShowSplitSections(isMeddiumScreen);
-
-    }, [windowWidth]);
-
-    const sections = {
-        firstTwo: ["Resumen de compra", "Datos de envío"],
-        lastTwo: ["Método de pago", "Confirmación"],
-    }
-
     return (
 
 
